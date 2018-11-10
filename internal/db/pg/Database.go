16122fb50bff5f8933c98471f64579d91dbd5a7f
Unknown <drdgvhbh@gmail.com>
2018-11-10 11:33:48 -0500

    Use an interface for sql db instead of the raw struct

--- internal/db/pg/Database.go
+++ internal/db/pg/Database.go
@@ -4,12 +4,16 @@ import (
 	"database/sql"
 )
 
+type SQLDb interface {
+	QueryRow(query string, args ...interface{}) *sql.Row
+}
+
 type Connector interface {
 	QueryRow(query string, args ...interface{}) Row
 }
 
 type connectorImp struct {
-	db *sql.DB
+	db SQLDb
 }
 
 func (connector connectorImp) QueryRow(query string, args ...interface{}) Row {
